42bc371f91f3219520fb563c19f96534650cf63c
Juan <33juanpintos33@gmail.com>
2024-05-25 18:32:02 -0300

    no se que hago

--- ObligatorioProgramacion3/obj/Debug/net8.0/ObligatorioProgramacion3.AssemblyInfo.cs
+++ ObligatorioProgramacion3/obj/Debug/net8.0/ObligatorioProgramacion3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb6b21d637077bc1b85a2d24f89ab5cdf2338cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4335a223f48063006aeb5c1b169838ec528417b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
