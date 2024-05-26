54856fa938baf29c80156a5ed33447a0415bc6f8
Juan <33juanpintos33@gmail.com>
2024-05-25 22:12:19 -0300

    se rompe solo esto

--- ObligatorioProgramacion3/obj/Debug/net8.0/ObligatorioProgramacion3.AssemblyInfo.cs
+++ ObligatorioProgramacion3/obj/Debug/net8.0/ObligatorioProgramacion3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8157d2bf28184e0df012f8386b375243f6f38b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+851382f5d3916d3964aa227d110414e7705a7e59")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
