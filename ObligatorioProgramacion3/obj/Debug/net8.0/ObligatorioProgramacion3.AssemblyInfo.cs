ab3c702efe5f4b46cfe5b91d7a486e9451537311
Juan <33juanpintos33@gmail.com>
2024-05-25 22:00:56 -0300

    asdasdasd

--- ObligatorioProgramacion3/obj/Debug/net8.0/ObligatorioProgramacion3.AssemblyInfo.cs
+++ ObligatorioProgramacion3/obj/Debug/net8.0/ObligatorioProgramacion3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cb9e0512f34490ac999051e9d76d4acd616b792")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fbcc905c32f7dab6e4d30f5def605ccff60b3cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
