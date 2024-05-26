ba9fcc41a0c815ac7192f14213280152de45c66c
Juan <33juanpintos33@gmail.com>
2024-05-25 22:06:27 -0300

    ya estoy perdido

--- ObligatorioProgramacion3/obj/Release/net8.0/ObligatorioProgramacion3.AssemblyInfo.cs
+++ ObligatorioProgramacion3/obj/Release/net8.0/ObligatorioProgramacion3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fbcc905c32f7dab6e4d30f5def605ccff60b3cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+becfb6c79b19c5b9fb8bb61d44387c6f98ff450e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProgramacion3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
