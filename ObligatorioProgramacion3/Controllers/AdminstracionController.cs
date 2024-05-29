0e83db05ead79424c114ad20d6db151e461481c1
Juan <33juanpintos33@gmail.com>
2024-05-29 02:47:45 -0300

    Cambios en general

---
+++ ObligatorioProgramacion3/Controllers/AdminstracionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ObligatorioProgramacion3.Controllers
+{
+    public class AdministracionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
