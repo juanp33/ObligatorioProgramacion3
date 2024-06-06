c70a18287a1105e0425e6cb84bb86cdeaa163075
antonio <antoniollado222@gmail.com>
2024-06-05 21:29:43 -0300

    kllklk

--- ObligatorioProgramacion3/Controllers/HomeController.cs
+++ ObligatorioProgramacion3/Controllers/HomeController.cs
@@ -34,7 +34,7 @@ namespace ObligatorioProgramacion3.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult Rese�as() {
+        public IActionResult Reseñas() {
             return View();
         }
     }
