21da179810b5d4ef13285c6c9e4745e394a43d65
Juan <33juanpintos33@gmail.com>
2024-06-08 07:09:29 -0300

    Cree paginas permiso

--- ObligatorioProgramacion3/Models/Permiso.cs
+++ ObligatorioProgramacion3/Models/Permiso.cs
@@ -7,7 +7,7 @@ public partial class Permiso
 {
     public int PermisoId { get; set; }
 
-    public string Nombre { get; set; } = null!;
+    public string NombrePagina { get; set; } = null!;
 
     public string? Descripcion { get; set; }
 
