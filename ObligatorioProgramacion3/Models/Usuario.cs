707b3f0e040bd60d76a91167fe197648e906c38f
guillermo <guillecorrea2001@gmail.com>
2024-06-20 17:00:29 -0300

    cambios usuario

--- ObligatorioProgramacion3/Models/Usuario.cs
+++ ObligatorioProgramacion3/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ObligatorioProgramacion3.Models;
 
@@ -7,14 +8,21 @@ public partial class Usuario
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres.")]
     public string Nombre { get; set; } = null!;
 
+    [Required(ErrorMessage = "El email es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El email no puede exceder los 100 caracteres.")]
+    [EmailAddress(ErrorMessage = "El email no tiene un formato válido.")]
     public string Email { get; set; } = null!;
 
+    [Required(ErrorMessage = "La contraseña es obligatoria.")]
+    [StringLength(100, ErrorMessage = "La contraseña no puede exceder los 100 caracteres.")]
     public string Contraseña { get; set; } = null!;
 
-    public int? RolId { get; set; }
 
+    public int? RolId { get; set; }
     public virtual Cliente? Cliente { get; set; }
 
     public virtual ICollection<Reserva> Reservas { get; set; } = new List<Reserva>();
